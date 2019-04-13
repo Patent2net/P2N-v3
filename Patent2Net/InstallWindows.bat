a700e553b0ecc41c92315c7f02bc0f9d7580ea07
Patent2net <patent2net@gmail.com>
2019-04-13 09:19:54 +0200

    BETA: trying an complete automation of install procedure
    not tested

--- Patent2Net/InstallWindows.bat
+++ Patent2Net/InstallWindows.bat
@@ -1,2 +1,3 @@
-for /F "delims=~" %%f in (Patent2Net\requirements.txt) do conda install --yes "%%f" 
+
+conda activate py36 & for /F "delims=~" %%f in (Patent2Net\requirements.txt) do conda install --yes "%%f" 
 
--- installP2N.bat
+++ installP2N.bat
@@ -1,2 +1,2 @@
-call Patent2Net\InstallWindows.bat 2> LogInstall1.txt & call Patent2Net\InstallWindows2.bat 2> LogInstall2.txt & python setup.py install > LogInstallP2N.txt
+conda create --name P2N python=3.6 & call Patent2Net\InstallWindows.bat 2> LogInstall1.txt & call Patent2Net\InstallWindows2.bat 2> LogInstall2.txt & conda activate P2N & python setup.py install > LogInstallP2N.txt
 
