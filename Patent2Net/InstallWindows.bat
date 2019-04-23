f43ef2f4ec5e73bad76f8a66ee54b1d1d526a0a9
Patent2net <patent2net@gmail.com>
2019-04-23 11:01:21 +0200

    Adapted install for Nezha's job
    various libraries more

--- Patent2Net/InstallWindows.bat
+++ Patent2Net/InstallWindows.bat
@@ -1,2 +1,1 @@
 conda activate P2N & for /F "delims=~" %%f in (Patent2Net\requirements.txt) do conda install --yes "%%f" 
-
--- segPhrases.py
+++ segPhrases.py
@@ -31,5 +31,4 @@ corpus = [
 # we need to pass splitted sentences to the model
 tokenized_sentences = [sentence.split() for sentence in corpus]
 
-model = word2vec.Word2Vec(tokenized_sentences, min_count=1)
 print(model)
