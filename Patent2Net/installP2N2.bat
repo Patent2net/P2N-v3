7cec74baa814b526726e563a4b6908de6154ab9d
Patent2net <patent2net@gmail.com>
2019-06-02 09:57:09 +0200

    Update installP2N2.bat
    mismatch caracter

--- Patent2Net/installP2N2.bat
+++ Patent2Net/installP2N2.bat
@@ -1,5 +1,5 @@
 (CALL conda update --yes -n base -c defaults conda
-CALL conda create --yes --name P2N2 python=3.6
+CALL conda create --yes --name P2N python=3.6
 CALL InstallWindows.bat 2> LogInstall1.txt
 CALL InstallWindows2.bat 2> LogInstall2.txt
 CALL conda activate P2N && python setup.py install > LogInstallP2N.txt
