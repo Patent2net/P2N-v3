62669b459120e6b905cadb7cec43d038d987b972
Patent2net <patent2net@gmail.com>
2019-06-05 18:38:55 +0200

    maj install (Nieme)

--- Patent2Net/InstallWindows.bat
+++ Patent2Net/InstallWindows.bat
@@ -1,1 +1,1 @@
-CALL conda.bat activate P2N && for /F "delims=~" %%f in (requirements.txt) do CALL conda install --yes "%%f" && CALL conda install --yes pygraphviz -c alubbock
+CALL conda.bat activate P2N && for /F "delims=~" %%f in (Patent2Net\requirements.txt) do CALL conda install --yes "%%f" && CALL conda install --yes pygraphviz -c alubbock
--- Patent2Net/installP2N2.bat
+++ Patent2Net/installP2N2.bat
@@ -1,10 +1,8 @@
 (CALL conda update --yes -n base -c defaults conda
 CALL conda create --yes --name P2N python=3.6
-cd Patent2Net
-CALL InstallWindows.bat 2> LogInstall1.txt
-CALL InstallWindows2.bat 2> LogInstall2.txt
+CALL Patent2Net\InstallWindows.bat 2> LogInstall1.txt
+CALL Patent2Net\InstallWindows2.bat 2> LogInstall2.txt
 CALL conda activate P2N
-cd ..
 python setup.py install > LogInstallP2N.txt
 )
 
