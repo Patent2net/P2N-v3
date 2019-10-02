9e7dc3164f8426a3ca3f98a51ad41162440d08f7
Patent2net <patent2net@gmail.com>
2019-10-02 09:21:43 +0200

    cleaning and debug git misuse

--- runP2N.bat
+++ runP2N.bat
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-conda activate P2N & for %%i in (REQUESTS/*.cql) do p2n run --config=../REQUESTS/%%i 
-=======
-conda activate P2N & for %%i in (REQUESTS/*.cql) do p2n run --config=../REQUESTS/%%i
->>>>>>> 975f1ff1dd681b27f377809d2cfa1f02f5d29b78
+conda activate P2N & for %%i in (REQUESTS/*.cql) do p2n run --config=../REQUESTS/%%i --with-family
 
