29404f6f753c5fe27cee960e072de9e92c2b02d7
Aziguy <monsieur_hernandez@hotmail.com>
2020-06-22 16:47:54 +0200

    Prétraitement_lentille..

--- Patent2Net/preProcessNormalisationApplicants.py
+++ Patent2Net/preProcessNormalisationApplicants.py
@@ -5,15 +5,16 @@ Created on Sat Dec 27 12:05:05 2014
 @author: dreymond
 """
 
-import json
+import datetime
 import os
 import sys
-#import pickle as pickle
-#import bs4
-from Patent2Net.P2N_Lib import UrlInventorBuild, UrlApplicantBuild, UrlIPCRBuild, UrlPatent, LoadBiblioFile, RenderTemplate
+
+import pandas as pd
+
 from Patent2Net.P2N_Config import LoadConfig
-import shutil
-import datetime
+# import bs4
+from Patent2Net.P2N_Lib import LoadBiblioFile
+
 aujourd = datetime.date.today()
 
 configFile = LoadConfig()
@@ -34,29 +35,34 @@ ResultListPath = configFile.ResultListPath
 ResultBiblioPath = configFile.ResultBiblioPath
 
 print("\n> Hi! This is Pre Process for normalizing applicant names: used on:", ndf)
-if 'Description'+ndf in os.listdir(ListBiblioPath):
-    with open(ListBiblioPath+'//'+ndf, 'r') as data:
+if 'Description' + ndf in os.listdir(ListBiblioPath):
+    with open(ListBiblioPath + '//' + ndf, 'r') as data:
         dico = LoadBiblioFile(ListBiblioPath, ndf)
-else: #Retrocompatibility
+else:  # Retrocompatibility
     print("please use Comptatibilizer")
     sys.exit()
 LstBrevet = dico['brevets']
 
+# Lecture du fichier de référence
+df = pd.read_csv('../Patent2Net/Resources/20200203_STAN.csv', sep=';', encoding='utf-8')
+
 for brev in LstBrevet:
-  
     # ici tu as accès au contenu d'un brevet.
     # le champ qui interesse c'est ['applicant']
-    temp = brev ['applicant']
-    
+    temp = brev['applicant']
+    for appli in temp:
+        for index, ligne in df.iterrows():
+            if appli in ligne['Variation Name']:
+                tempo = ligne['univ'].index(appli)
+            else:
+                pass
 
     # sauvegarde dans un fichier tempo
-    with open(ResultBiblioPath + '//tempo' + ndf, 'ab') as ficRes:
-        pickle.dump(brevet, ficRes)
+    # with open(ResultBiblioPath + '//tempo' + ndf, 'ab') as ficRes:
+    # pickle.dump(brev, ficRes)
 
 
 # remplacement de la source par le résultat
 # à n'activer que quand çà marche ^_^
 # os.remove(ResultBiblioPath + '//' + ndf)
 # shutil.move(ResultBiblioPath + '//tempo' + ndf, ResultBiblioPath + '//' + ndf)
-
-
