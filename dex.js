c4a6c0037a784f4e19b1988c3c1b313e68fe94aa
Patent2net <patent2net@gmail.com>
2020-09-10 17:46:36 +0200

    Update dex.js

--- dex.js
+++ dex.js
@@ -1,33 +1,5 @@
 document.write('\
 <ul>\
-<li><a href="DATA/lentille671.html" target="_blank">lentille671</a></li>\
-<li><a href="DATA/lentille670.html" target="_blank">lentille670</a></li>\
-<li><a href="DATA/lentille67.html" target="_blank">lentille67</a></li>\
-<li><a href="DATA/lentille66.html" target="_blank">lentille66</a></li>\
-<li><a href="DATA/lentille6.html" target="_blank">lentille6</a></li>\
-<li><a href="DATA/lentille5.html" target="_blank">lentille5</a></li>\
-<li><a href="DATA/lentille4.html" target="_blank">lentille4</a></li>\
-<li><a href="DATA/lentille2.html" target="_blank">lentille2</a></li>\
 <li><a href="DATA/lentille.html" target="_blank">lentille</a></li>\
-<<<<<<< Updated upstream
-<li><a href="DATA/P7-LargebisPublic.html" target="_blank">P7-LargebisPublic</a></li>\
-<li><a href="DATA/P7-LargebisMix.html" target="_blank">P7-LargebisMix</a></li>\
-<li><a href="DATA/P7-LargebisIndus.html" target="_blank">P7-LargebisIndus</a></li>\
-<li><a href="DATA/P7-LargebisAutres.html" target="_blank">P7-LargebisAutres</a></li>\
-<li><a href="DATA/P7-Largebis.html" target="_blank">P7-Largebis</a></li>\
-<li><a href="DATA/P7-LargePublic.html" target="_blank">P7-LargePublic</a></li>\
-<li><a href="DATA/P7-LargeMix.html" target="_blank">P7-LargeMix</a></li>\
-<li><a href="DATA/P7-LargeIndus.html" target="_blank">P7-LargeIndus</a></li>\
-<li><a href="DATA/P7-LargeAutres.html" target="_blank">P7-LargeAutres</a></li>\
-<li><a href="DATA/P7-Large.html" target="_blank">P7-Large</a></li>\
-<li><a href="DATA/P7-UnivPublic.html" target="_blank">P7-UnivPublic</a></li>\
-<li><a href="DATA/P7-UnivMix.html" target="_blank">P7-UnivMix</a></li>\
-<li><a href="DATA/P7-UnivIndus.html" target="_blank">P7-UnivIndus</a></li>\
-<li><a href="DATA/P7-UnivAutres.html" target="_blank">P7-UnivAutres</a></li>\
-<li><a href="DATA/P7-Univ.html" target="_blank">P7-Univ</a></li>\
-<li><a href="DATA/Lentille.html" target="_blank">Lentille</a></li>\
-<li><a href="DATA/Datte.html" target="_blank">Datte</a></li>\
-=======
->>>>>>> Stashed changes
 </ul>\
 ');
