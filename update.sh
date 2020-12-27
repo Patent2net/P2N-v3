0e3aea10191d0defbbac08b1db10affca0e5825f
Patent2net <patent2net@gmail.com>
2020-12-27 19:13:29 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,5 +2,5 @@ git stash
 git pull $@
 python setup.py build
 python setup.py install
-python Patent2Net/extensions/Dlcarrot2.py /usr/src
+python Patent2Net/extensions/Dlcarrot2.py /usr/src/P2N-V3
 chmod +x update.sh
