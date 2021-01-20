6a66822e7455c2a26d485fe1945b13408fe5dc4d
Patent2net <patent2net@gmail.com>
2021-01-20 08:58:02 +0100

    cleaning (again)

--- Patent2Net/obsolete/Patent2Net/Patent2Net-bak/InstallWindows2.bat
+++
@@ -1,2 +0,0 @@
-for /F "delims=~" %%f in (Patent2Net\requirementsPip.txt) do pip install --upgrade "%%f" && python -m textblob.download_corpora
-
--- Patent2Net/obsolete/runP2NSpecial.bat
+++
@@ -1,3 +0,0 @@
-conda activate P2N & for %%f in (Requests\*.cql) do (
-move /Y %%f requete.cql & p2n acquire --config=../requete.cql
-)
