88200be9b8e75a26ae32bae70773ef9a4afc2293
Patent2net <patent2net@gmail.com>
2021-01-20 09:47:09 +0100

    clean again

--- carrot2.sh
+++
@@ -1,9 +0,0 @@
-python Patent2Net/extensions/Dlcarrot2.py /usr/src/P2N-V3
-chown -R root:ftp carrot2.zip
-chgrp -R ftp carrot2.zip
-unzip -o carrot2.zip -d carrot2
-rm carrot2.zip
-chgrp -R ftp carrot2*
-chmod -R 755 carrot2/carrot2-4.0.4/dcs/dcs.sh
-
-
