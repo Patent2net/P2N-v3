389e5131b95439ee1f3a2ecfd6d7aadaa1607b43
Patent2net <patent2net@gmail.com>
2021-01-24 12:12:42 +0100

    Net NEts and stats more reliable

--- Patent2Net/scriptP7.ps1
+++ Patent2Net/scriptP7.ps1
@@ -1,9 +1,13 @@
-$Reps = "requests2"
+# directory containing requests files
+
+$Reps = "requests2", "requests3", "requests1", "REQ-P7"
+
+# processing networks and reseting the whole interface
+
 foreach ($scriptPath in $Reps) {
 	$scriptPath = (-join('..\', $scriptPath, '\'))
 	echo $scriptPath
 	$filename = Get-ChildItem ($scriptPath)
-	foreach ($f in $filename) {python AnalyseReseaux2.py (-join($scriptPath,$f))}
-	foreach ($f in $filename) {foreach ($net in (Get-Content .\nets.txt)) {python P2N-NetworksBis.py $net --config=(-join($scriptPath,$f))}}
-	foreach ($f in $filename) {foreach ($app in (Get-Content .\scriptlist.txt)) {python $app --config=(-join($scriptPath,$f))}}
+    #foreach ($f in $filename) {foreach ($app in (Get-Content .\scriptlist.txt)) {python $app --config=(-join($scriptPath,$f))}}
+	foreach ($f in $filename) {foreach ($app in ("AcadNNets.py", "Interface2.py")) {python $app --config=(-join($scriptPath,$f))}}
 	}
