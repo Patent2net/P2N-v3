1f0a2c7ef617707fe158fae5719e5c7f4941fd90
Patent2net <patent2net@gmail.com>
2021-02-03 06:46:53 +0100

    Update dex.js

--- dex.js
+++ dex.js
@@ -1,8 +1,4 @@
 document.write('\
 <ul>\
-<li><a href="DATA/IA.html" target="_blank">IA</a></li>\
-<li><a href="DATA/P7-Univ.html" target="_blank">P7-Univ</a></li>\
-<li><a href="DATA/P7-Largebis.html" target="_blank">P7-Largebis</a></li>\
-<li><a href="DATA/P7-Large.html" target="_blank">P7-Large</a></li>\
 </ul>\
 ');
