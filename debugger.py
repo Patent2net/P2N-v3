89e9676d526c1baf7cbe90b6f85ee5da3a890cca
romaric-g <romaric.gauzi1@gmail.com>
2021-04-21 16:54:58 +0200

    change

---
+++ debugger.py
@@ -0,0 +1,13 @@
+from os import getenv
+
+def initialize_flask_server_debugger_if_needed():
+    if getenv("DEBUGGER") == "True":
+        import multiprocessing
+
+        if multiprocessing.current_process().pid > 1:
+            import debugpy
+
+            debugpy.listen(("0.0.0.0", 10001))
+            print("⏳ VS Code debugger can now be attached, press F5 in VS Code ⏳", flush=True)
+            debugpy.wait_for_client()
+            print("🎉 VS Code debugger attached, enjoy debugging 🎉", flush=True)
--- dex.js
+++ dex.js
@@ -1,4 +1,4 @@
-document.write('\
+document.write('\ 
 <ul>\
 </ul>\
 ');
