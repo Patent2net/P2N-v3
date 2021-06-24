8acb478f7e26354e66fc497787e083b71673dabf
Patent2net <patent2net@gmail.com>
2021-06-24 16:32:38 +0200

    no .sh

--- carrot2.sh
+++ carrot2.sh
@@ -4,6 +4,7 @@ chgrp -R ftp /home/p2n/carrot2.zip
 unzip -o /home/p2n/carrot2.zip -d /home/p2n/carrot2
 rm /home/p2n/carrot2.zip
 chgrp -R ftp /home/p2n/carrot2*
-chmod -R 755 /home/p2n/carrot2/carrot2-4.2.1/dcs/dcs.sh
+chmod -R 755 /home/p2n/carrot2/carrot2-4.2.1/dcs/dcs
+
 
 
