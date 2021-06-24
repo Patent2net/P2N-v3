f98be0311b692e096360f54d22ddee702205c4fe
Patent2net <patent2net@gmail.com>
2021-06-24 05:55:04 +0200

    Update carrot2.sh

--- carrot2.sh
+++ carrot2.sh
@@ -1,9 +1,9 @@
-python Patent2Net/extensions/Dlcarrot2.py /usr/src/P2N-V3
-chown -R root:ftp carrot2.zip
-chgrp -R ftp carrot2.zip
-unzip -o carrot2.zip -d carrot2
-rm carrot2.zip
-chgrp -R ftp carrot2*
-chmod -R 755 carrot2/carrot2-4.2.1/dcs/dcs.sh
+python Patent2Net/extensions/Dlcarrot2.py /home/p2n/
+chown -R root:ftp /home/p2n/carrot2.zip
+chgrp -R ftp /home/p2n/carrot2.zip
+unzip -o /home/p2n/carrot2.zip -d /home/p2n/carrot2
+rm /home/p2n/carrot2.zip
+chgrp -R ftp /home/p2n/carrot2*
+chmod -R 755 /home/p2n/carrot2/carrot2-4.2.1/dcs/dcs.sh
 
 
