7a65b5f25a10a10e8e27439867f9fa6ded939119
Patent2net <patent2net@gmail.com>
2021-09-23 15:24:08 +0200

    interface fixes

--- web_app/src/pages/Request/Header/index.js
+++ web_app/src/pages/Request/Header/index.js
@@ -19,6 +19,14 @@ const RequestHeader = ({ dir, data, showLoading }) => {
         });
     }, [dir])
 
+    const updateRequest = React.useCallback(() => {
+        fetch('http://localhost:5000/api/v1/requests/' + dir+'/update', {
+            method: 'POST'
+        })
+        .then(function(response) {
+            history.push('/app/requests')
+        })
+    }, [dir, history])
 
     const deleteRequest = React.useCallback(() => {
         fetch('http://localhost:5000/api/v1/requests/' + dir, { 
@@ -55,6 +63,15 @@ const RequestHeader = ({ dir, data, showLoading }) => {
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                     </svg>
                 </button>
+
+                <button
+                    className="focus:outline-none p-2 rounded-md font-semibold text-white bg-green-500 mr-2 flex justify-center items-center"
+                    onClick={updateRequest}
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                    </svg>
+                </button>
                 { 
                     data.done && (
                         <>
@@ -81,7 +98,7 @@ const RequestHeader = ({ dir, data, showLoading }) => {
                                 href={"/DATA/" + dir + ".html"} 
                                 className="focus:outline-none px-8 py-2 rounded-md font-semibold text-white bg-indigo-500"
                             >
-                                Voir les données
+                                See data
                             </a>
                         </>
                     ) 
@@ -95,7 +112,7 @@ const RequestHeader = ({ dir, data, showLoading }) => {
                                 <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                                 <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                             </svg>
-                            Chargements
+                            Loading
                         </div>
                     )
                 }
