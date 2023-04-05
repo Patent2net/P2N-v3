c2dcf7453aa9073a4b4999f4e55549d406b231e8
Patent2net <patent2net@gmail.com>
2023-04-05 17:43:59 +0200

    update for new image

--- carrot2.sh
+++ carrot2.sh
@@ -1,9 +1,7 @@
+source /vpy3/bin/activate
 python Patent2Net/extensions/Dlcarrot2.py /home/p2n/
-chown -R root:ftp /home/p2n/carrot2.zip
-chgrp -R ftp /home/p2n/carrot2.zip
 unzip -o /home/p2n/carrot2.zip -d /home/p2n/carrot2
 rm /home/p2n/carrot2.zip
-chgrp -R ftp /home/p2n/carrot2*
 chmod -R 755 /home/p2n/carrot2/carrot2-4.2.1/dcs/dcs
 
 
